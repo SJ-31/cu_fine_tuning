c2bdfcc25fc6673b0cf2a1b4039627715b399896
Shann Chongwattanankul <shanj3131@gmail.com>
2026-08-21 14:02:53 +0700

    chore: explicitly write review file and remove exact case matches

--- scripts/map_diseases_mondo.py
+++ scripts/map_diseases_mondo.py
@@ -17,16 +17,21 @@ def lookup_safe(x: str, ont: SearchOntology) -> str | None:
         pass
 
 
-mapped_file = here("data", "processed", "mondo_disease_mapping.csv")
+tmp_file = here("data", "processed", "mondo_disease_mapping_tmp.csv")
 failure_file: Path = here("data", "processed", "ols_failed_lookups.txt")
+
+fill_custom = here("data", "processed", "mondo_disease_mapping_review.csv")
+
 if failure_file.exists():
     failures: list = failure_file.read_text().splitlines()
 else:
     failures = []
-if mapped_file.exists():
-    mappings = pl.read_csv(mapped_file).filter(
+if tmp_file.exists():
+    mappings = pl.read_csv(tmp_file).filter(
         (pl.col("disease").is_not_null()) & (pl.col("mondo").is_not_null())
     )
+    if fill_custom.exists():
+        mappings = pl.concat([mappings, pl.read_csv(fill_custom)])
 else:
     mappings = pl.DataFrame({"disease": [], "mondo": []})
 df = (
@@ -41,6 +46,7 @@ excluded = [
     "See cases",
     "non-transition zone (non-transition zone)",
     "no recurrence (no recurrence)",
+    ".",
 ]
 diseases = (
     df["disease"]
@@ -52,6 +58,7 @@ diseases = (
     .filter(
         (pl.col("disease").str.len_chars() > 0)
         & (~pl.col("disease").is_in(excluded))
+        & (~pl.col("disease").is_in(failures))
         & (~pl.col("disease").is_in(mappings["disease"]))
     )
 )
@@ -62,7 +69,8 @@ ontology = SearchOntology(
 from_ontology = diseases.with_columns(
     pl.Series([lookup_safe(x, ontology) for x in diseases["disease"]]).alias("mondo")
 ).select(["disease", "mondo"])
-from_ontology.write_csv(mapped_file)
+to_join = from_ontology.filter(~pl.col("mondo").is_null())
+
 
 lookups_left = from_ontology.filter(pl.col("mondo").is_null())
 ols = SearchOLS(["mondo"], cache=here("data", "ols_cache.db"))
@@ -71,7 +79,33 @@ for d in lookups_left["disease"]:
     try:
         df = ols.search(d)
         results.append(df)
-    except:
+    except Exception as e:
+        print(f"Exception when looking up {d}: {e}")
         failures.append(d)
-pl.concat(results).write_csv(here("data", "processed", "mondo_ols_lookups.csv"))
+
+ols_df = pl.concat(results).with_columns(pl.col("description").list.first())
+case_same = (
+    ols_df.filter(
+        (pl.col("label").str.to_lowercase() == pl.col("query").str.to_lowercase())
+        | (
+            pl.col("query")
+            .str.to_lowercase()
+            .is_in(pl.col("exact_synonyms").list.eval(pl.element().str.to_lowercase()))
+        )
+    )
+    .select(["query", "obo_id"])
+    .rename({"query": "disease", "obo_id": "mondo"})
+)
+ols_df = ols_df.filter(~pl.col("query").is_in(case_same["disease"]))
+
+# Write all files
+#
+ols_df.with_columns(pl.col("exact_synonyms").list.join(";")).write_csv(
+    here("data", "processed", "mondo_ols_lookups.csv")
+)
+pl.concat([mappings, to_join, case_same], how="diagonal_relaxed").write_csv(tmp_file)
+lookups_left.filter(
+    (~pl.col("disease").is_in(failures))
+    & (~pl.col("disease").is_in(case_same["disease"]))
+).write_csv(fill_custom)
 failure_file.write_text("\n".join(failures))
