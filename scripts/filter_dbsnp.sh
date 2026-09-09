0bf8a1d2dc4f4dbe21b1c31d9fde7ce07d733897
Shann Chongwattanankul <shanj3131@gmail.com>
2026-09-09 15:45:00 +0700

    chore: add script to get benign variants from dbsnp

---
+++ scripts/filter_dbsnp.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+dbsnp="/data/project/stemcell/shannc/reference/variants/dbSNP_renamed.vcf.gz"
+exprs="(INFO/CLNSIG ~ '2' || INFO/CLNSIG ~ '3') && (INFO/CLNREVSTAT ~ 'mult' || INFO/CLNREVSTAT ~ 'exp' || INFO/CLNREVSTAT ~ 'practice') && INFO/VC == 'SNV'"
+output="/data/project/stemcell/shannc/repos/evo2_fine_tune/raw/dbSNP_benign.csv"
+query="%INFO/VC,%INFO/GENEINFO,%INFO/FREQ,%INFO/CLNHGVS,%INFO/CLNSIG"
+
+echo "vc,geneinfo,freq,hgvs,clinsig" >"${output}"
+bcftools filter -i "${exprs}" "${dbsnp}" |
+	bcftools query -f "${query}" >>"${output}"
