aacfb88d715b12377ed7366a6a6c1958a2b2374c
Shann Chongwattanankul <shanj3131@gmail.com>
2026-09-10 13:41:45 +0700

    chore: store dbsnp id

--- scripts/filter_dbsnp.sh
+++ scripts/filter_dbsnp.sh
@@ -3,8 +3,8 @@
 dbsnp="/data/project/stemcell/shannc/reference/variants/dbSNP_renamed.vcf.gz"
 exprs="(INFO/CLNSIG ~ '2' || INFO/CLNSIG ~ '3') && (INFO/CLNREVSTAT ~ 'mult' || INFO/CLNREVSTAT ~ 'exp' || INFO/CLNREVSTAT ~ 'practice') && INFO/VC == 'SNV'"
 output="/data/project/stemcell/shannc/repos/evo2_fine_tune/raw/dbSNP_benign.tsv"
-query="%INFO/VC\t%INFO/GENEINFO\t%INFO/FREQ\t%INFO/CLNHGVS\t%INFO/CLNSIG"
+query="%ID\t%INFO/VC\t%INFO/GENEINFO\t%INFO/FREQ\t%INFO/CLNHGVS\t%INFO/CLNSIG"
 
-echo -e "vc\tgeneinfo\tfreq\thgvs\tclinsig" >"${output}"
+echo -e "id\tvc\tgeneinfo\tfreq\thgvs\tclinsig" >"${output}"
 bcftools filter -i "${exprs}" "${dbsnp}" |
 	bcftools query -f "${query}" >>"${output}"
